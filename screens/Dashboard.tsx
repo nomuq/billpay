23f1cff2efdfb8a0641fec7bc33a84d6e28bfa49
Satish Babariya <satish.babariya@gmail.com>
2021-11-29 17:54:35 +0530

    minor ui fix

--- screens/Dashboard.tsx
+++ screens/Dashboard.tsx
@@ -17,202 +17,10 @@ import {
 import { MaterialIcons } from "@expo/vector-icons";
 
 export default function Dashboard({ navigation }: any) {
-  const insets = useSafeAreaInsets();
-  const { height, width } = useWindowDimensions();
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
-        <View style={styles.header}>
-          <View
-            style={{
-              paddingTop: insets.top,
-              paddingBottom: insets.bottom,
-              flex: 1,
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <View
-              style={{
-                flex: 1.8,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "baseline",
-                }}
-              >
-                <Text
-                  style={{
-                    fontSize: PixelRatio.getFontScale() * 30,
-                    color: "#ffffff99",
-                    fontFamily: "OpenSans-Regular",
-                    paddingRight: 2,
-                  }}
-                >
-                  {"\u20B9"}
-                </Text>
-                <Text
-                  style={{
-                    fontSize: PixelRatio.getFontScale() * 38,
-                    color: Colors.dark.text,
-                    fontFamily: "OpenSans-Bold",
-                  }}
-                >
-                  14,450.00
-                </Text>
-              </View>
-              <Text
-                style={{
-                  fontSize: PixelRatio.getFontScale() * 12,
-                  color: "#ffffff99",
-                  fontFamily: "OpenSans-Regular",
-                }}
-              >
-                Total Expanse this month (7)
-              </Text>
-            </View>
-            <View
-              style={{
-                flex: 1,
-              }}
-            >
-              <View
-                style={{
-                  paddingBottom: 10,
-                }}
-              >
-                <ProgressBar
-                  progress={0.3}
-                  color={"#16c591"}
-                  style={{
-                    backgroundColor: "#fff",
-                    borderRadius: 10,
-                    height: 10,
-                    width: width * 0.85,
-                  }}
-                />
-              </View>
-              <View
-                style={{
-                  flex: 1,
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  marginHorizontal: 10,
-                }}
-              >
-                <View>
-                  <View
-                    style={{
-                      flexDirection: "row",
-                      alignItems: "baseline",
-                      paddingVertical: 2,
-                    }}
-                  >
-                    <Text
-                      style={{
-                        fontSize: PixelRatio.getFontScale() * 16,
-                        color: "#ffffff99",
-                        fontFamily: "OpenSans-Regular",
-                        paddingRight: 2,
-                      }}
-                    >
-                      {"\u20B9"}
-                    </Text>
-                    <Text
-                      style={{
-                        fontSize: PixelRatio.getFontScale() * 17,
-                        color: "#ffffff",
-                        fontFamily: "OpenSans-SemiBold",
-                      }}
-                    >
-                      1,500.00
-                    </Text>
-                  </View>
-                  <View
-                    style={{
-                      flexDirection: "row",
-                      alignItems: "baseline",
-                      paddingVertical: 4,
-                    }}
-                  >
-                    <MaterialIcons
-                      name="north-east"
-                      color={"#16c591"}
-                      size={PixelRatio.getFontScale() * 13}
-                      style={{ alignSelf: "center", paddingRight: 3 }}
-                    />
-                    <Text
-                      style={{
-                        fontSize: PixelRatio.getFontScale() * 13,
-                        color: "#ffffff99",
-                        fontFamily: "OpenSans-Regular",
-                      }}
-                    >
-                      Paid (1)
-                    </Text>
-                  </View>
-                </View>
-                <View>
-                  <View
-                    style={{
-                      flexDirection: "row",
-                      alignItems: "baseline",
-                      paddingVertical: 4,
-                    }}
-                  >
-                    <Text
-                      style={{
-                        fontSize: PixelRatio.getFontScale() * 16,
-                        color: "#ffffff99",
-                        fontFamily: "OpenSans-Regular",
-                        paddingRight: 2,
-                      }}
-                    >
-                      {"\u20B9"}
-                    </Text>
-                    <Text
-                      style={{
-                        fontSize: PixelRatio.getFontScale() * 17,
-                        color: "#ffffff",
-                        fontFamily: "OpenSans-SemiBold",
-                      }}
-                    >
-                      12,950.00
-                    </Text>
-                  </View>
-                  <View
-                    style={{
-                      flexDirection: "row",
-                      alignItems: "baseline",
-                      paddingVertical: 4,
-                    }}
-                  >
-                    <MaterialIcons
-                      name="priority-high"
-                      color={"#fb7c75"}
-                      size={PixelRatio.getFontScale() * 13}
-                      style={{ alignSelf: "center", paddingRight: 3 }}
-                    />
-                    <Text
-                      style={{
-                        fontSize: PixelRatio.getFontScale() * 13,
-                        color: "#ffffff99",
-                        fontFamily: "OpenSans-Regular",
-                      }}
-                    >
-                      Due (6)
-                    </Text>
-                  </View>
-                </View>
-              </View>
-            </View>
-          </View>
-        </View>
+        <Header />
         <View style={styles.list}></View>
       </View>
     </SafeAreaView>
@@ -239,3 +47,167 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.background,
   },
 });
+
+function Header({ navigation }: any) {
+  const insets = useSafeAreaInsets();
+  const { height, width } = useWindowDimensions();
+  return (
+    <View style={styles.header}>
+      <View
+        style={{
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+          flex: 1,
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            flex: 1.8,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "baseline",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 30,
+                color: "#ffffff99",
+                fontFamily: "OpenSans-Regular",
+                paddingRight: 2,
+              }}
+            >
+              {"\u20B9"}
+            </Text>
+            <Text
+              style={{
+                fontSize: PixelRatio.getFontScale() * 35,
+                color: Colors.dark.text,
+                fontFamily: "OpenSans-Bold",
+              }}
+            >
+              14,450.00
+            </Text>
+          </View>
+          <Text
+            style={{
+              fontSize: 12,
+              color: "#ffffff99",
+              fontFamily: "OpenSans-Regular",
+            }}
+          >
+            Total Expanse this month (7)
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+          }}
+        >
+          <View
+            style={{
+              paddingBottom: 10,
+            }}
+          >
+            <ProgressBar
+              progress={0.3}
+              color={"#16c591"}
+              style={{
+                backgroundColor: "#fff",
+                borderRadius: 10,
+                height: 10,
+                width: width * 0.85,
+                elevation: 5,
+                zIndex: 1,
+              }}
+            />
+          </View>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginHorizontal: 10,
+            }}
+          >
+            <StatsComponent
+              value="1,500.00"
+              icon="north-east"
+              icon_color="#16c591"
+            />
+
+            <StatsComponent
+              value="12,950.00"
+              icon="priority-high"
+              icon_color="#fb7c75"
+            />
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+function StatsComponent({ value, icon, icon_color }: any) {
+  return (
+    <View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "baseline",
+          paddingVertical: 2,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 16,
+            color: "#ffffff99",
+            fontFamily: "OpenSans-Regular",
+            paddingRight: 2,
+          }}
+        >
+          {"\u20B9"}
+        </Text>
+        <Text
+          style={{
+            fontSize: 17,
+            color: "#ffffff",
+            fontFamily: "OpenSans-SemiBold",
+          }}
+        >
+          {value}
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "baseline",
+          paddingVertical: 4,
+        }}
+      >
+        <MaterialIcons
+          name={icon}
+          color={icon_color}
+          size={13}
+          style={{ alignSelf: "center", paddingRight: 3 }}
+        />
+        <Text
+          style={{
+            fontSize: 13,
+            color: "#ffffff99",
+            fontFamily: "OpenSans-Regular",
+          }}
+        >
+          Paid (1)
+        </Text>
+      </View>
+    </View>
+  );
+}
